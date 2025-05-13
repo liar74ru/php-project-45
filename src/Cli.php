0b80221fefb3ee7948ac938a401fb55baee4fb60
Vladimir Kipriyanov <liar2707@gmail.com>
2025-05-13 19:44:50 +0500

    fix Review ver 1.2

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function getShowName(): string               // Знакомимся с пользователем и приветствуем его
 {
-    line("Welcome to the Brain Game!");
+    line("Welcome to the Brain Games!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
